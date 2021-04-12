f112445a9da9b4ee731974ce1bec10286da84d8f
Andrew Meyer <andrew.meyer@mongodb.com>
2021-04-12 20:03:14 +0200

    Generate a static schema property from property decorators that get the typescript type

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,37 @@
 import "reflect-metadata";
 
-const typeMetadataKey = Symbol("type");
-function property(type: string) {
-  return Reflect.metadata(typeMetadataKey, type);
-}
+const properties = {} 
 
-function getType(target: any, propertyKey: string) {
-  return Reflect.getMetadata(typeMetadataKey, target, propertyKey);
-}
+function property(
+    target: any,
+    key: string,
+  ) {
+    var t = Reflect.getMetadata("design:type", target, key);
+    properties[key] = t.name
+  }
 
-function test(C: any):any {
-    console.log("where am i called?")
-    return class extends C{
-            constructor(){
-                super()
-                this.a=3
-            }
-            static schema = {value:"test"}
-        }
-}
+type Constructor = { new (...args: any[]): any };
 
-@test
-class A {
-    @property("number")
-    a:number 
-
-    constructor(){
-        this.a = 2
+function realm<T extends Constructor>(BaseClass: T) {
+    return class extends BaseClass {
+        static schema = {properties}
     }
 }
+@realm
+class A{
+    @property
+    count: number 
+    @property
+    name: string
 
-//@ts-ignore
-console.log("before instantiation", A.schema)
-const a = new A()
-//@ts-ignore
-console.log("after:", a.schema)
-console.log(a.a)
+    constructor(){
+        this.count = 2
+        this.name = 'andrew'
+    }
 
+    static schema = {}
+}
+
+const obj = new A()
+
+console.log("schema", A.schema)  // schema { properties: { count: 'Number', name: 'String' } }
