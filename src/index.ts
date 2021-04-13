7c1ce93a2de79f4f338dfc1f56b48e5da0e41596
Andrew Meyer <andrew.meyer@mongodb.com>
2021-04-13 14:15:53 +0200

    Get things working

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,137 @@
+import { ObjectId } from "bson";
 import "reflect-metadata";
 
-const properties = {} 
+const _schemas = {} 
 
-function property(
+// function property(
+//     target: any,
+//     key: string,
+//   ) {
+//       console.log(`target: ${target.constructor.name}` )
+//     const type = Reflect.getMetadata("design:type", target, key);
+//     //properties[key] = type?.name
+//   }
+
+  interface AttributeProperties{
+    type: string,
+    optional?: boolean,
+    linkingObject?: string,
+    property?: string
+  }
+
+  /**
+   * 
+    name: 'TodoItem',
+    properties: {
+      _id: 'objectId',
+      description: 'string',
+      done: {type: 'bool', default: false},
+      lists: {
+        type: 'linkingObjects',
+        objectType: 'TodoList',
+        property: 'items',
+      },
+      deadline: 'date?',
+    },
+    primaryKey: '_id',
+   */
+
+function property(properties?: AttributeProperties) {
+    return function(target:any, key:string){
+        const schema = getSchemaFromTarget(target)
+        const type = Reflect.getMetadata("design:type", target, key);
+        schema.properties[key] = {type: type?.name, ...properties }
+    }
+  }
+
+  function getSchemaFromTarget(target){
+    if(_schemas[target.constructor.name] == undefined){
+        _schemas[target.constructor.name] = {
+            name: target.constructor.name,
+            properties: {}
+
+        }
+    }
+    return _schemas[target.constructor.name]
+  }
+
+function primaryKey(
     target: any,
     key: string,
   ) {
-    var t = Reflect.getMetadata("design:type", target, key);
-    properties[key] = t.name
+        const schema = getSchemaFromTarget(target)
+        schema.primaryKey = key
   }
+
+function linked(properties?: AttributeProperties) {
+    return function(target:any, key:string){
+        const schema = getSchemaFromTarget(target)
+        const type = Reflect.getMetadata("design:type", target, key);
+        schema.properties[key] = {type: properties?.type || type?.name, optional: properties?.optional || false }
+    }
+  }
+
 
 type Constructor = { new (...args: any[]): any };
 
+type Float = number
+
 function realm<T extends Constructor>(BaseClass: T) {
+    console.log("here first?")
+    //@ts-ignore
+    const test = Reflect.getMetadata("design:type", BaseClass, "count") 
+    console.log("my type: ", test?.name)
     return class extends BaseClass {
-        static schema = {properties}
+        static schema = _schemas[BaseClass.name]
     }
 }
+
+class Collection{}
+
 @realm
 class A{
-    @property
-    count: number 
-    @property
-    name: string
+    @property() @primaryKey _id: ObjectId
+    @property({type: 'double', linkingObject: 'B', property: 'items' }) @primaryKey count: number 
+    @property() weight?: Float
+    @property() name: string[]
+
+    primaryKey?: string
 
     constructor(){
+        this._id = new ObjectId()
         this.count = 2
-        this.name = 'andrew'
+        //this.weight = 2.2
+        this.name = ['andrew']
     }
 
     static schema = {}
 }
 
+@realm
+class B{
+    @property({type: 'ObjectId', optional: true}) @primaryKey _id: ObjectId
+    @property() count: number 
+    @property() weight?: Float
+    @property() name: string[]
+
+    primaryKey?: string
+
+    constructor(){
+        this._id = new ObjectId()
+        this.count = 2
+        //this.weight = 2.2
+        this.name = ['andrew']
+    }
+
+    
+
+    static schema = {}
+
+}
+
 const obj = new A()
 
-console.log("schema", A.schema)  // schema { properties: { count: 'Number', name: 'String' } }
+console.log("A schema", A.schema)  // schema { properties: { count: 'Number', name: 'String' } }
+
+const obj2 = new B()
+console.log("B schema", B.schema)  // schema { properties: { count: 'Number', name: 'String' } }
