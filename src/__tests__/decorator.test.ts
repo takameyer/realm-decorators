d4ffa7a3d42fc2dda0982a78944161602101b890
Steffen Agger <steffen.agger@gmail.com>
2021-04-16 18:46:46 +0200

    eslint added, prettier run through eslint, fixed errors/warnings.

--- src/__tests__/decorator.test.ts
+++ src/__tests__/decorator.test.ts
@@ -1,90 +1,90 @@
-import { Decimal128, ObjectId, UUID } from "bson";
-import { classModel, linkedTo, property } from "../";
-@classModel({ name: "", embedded: true })
+import { Decimal128, ObjectId, UUID } from 'bson'
+import { classModel, linkedTo, property } from '../'
+@classModel({ name: '', embedded: true })
 class EmbeddedModel {
   @property()
-  name: string;
+  name: string
 }
 
 @classModel()
 class TestModel {
   @property({ primary: true })
-  _id: ObjectId;
+  _id: ObjectId
 
-  @property({ type: "int", indexed: true })
-  manNumber: number;
+  @property({ type: 'int', indexed: true })
+  manNumber: number
 
   @property()
-  manString: string;
+  manString: string
 
   @property({ optional: true })
-  optDecimal?: Decimal128;
+  optDecimal?: Decimal128
 
   @property({ optional: true })
-  optUuid?: UUID;
+  optUuid?: UUID
 
   @property({ optional: true })
-  optBool?: boolean;
+  optBool?: boolean
 
-  @property({ type: "string[]?" })
-  optStringList?: Array<string>;
+  @property({ type: 'string[]?' })
+  optStringList?: Array<string>
 
-  @property({ type: "EmbeddedModel[]?" })
-  optEmbeddedObjects?: Realm.List<EmbeddedModel>;
+  @property({ type: 'EmbeddedModel[]?' })
+  optEmbeddedObjects?: Realm.List<EmbeddedModel>
 
-  @property({ type: "Item" })
-  items?: any;
+  @property({ type: 'Item[]' })
+  items?: Realm.List<Item>
 
   constructor(num: number, str: string) {
-    this.manNumber = num;
-    this.manString = str;
+    this.manNumber = num
+    this.manString = str
   }
 }
 
 @classModel()
 class Item {
   @property({ primary: true })
-  _id: ObjectId;
+  _id: ObjectId
 
-  @linkedTo(TestModel, "items")
-  models: any;
+  @linkedTo(TestModel, 'items')
+  models: Realm.Results<TestModel>
 }
 
-describe("decorator", () => {
-  it("to work as expected", () => {
+describe('decorator', () => {
+  it('to work as expected', () => {
     // @ts-ignore
     expect(TestModel.schema).toStrictEqual({
       properties: {
-        _id: { type: "objectId", indexed: true },
-        manNumber: { type: "int", indexed: true },
-        manString: { type: "string" },
-        optDecimal: { type: "decimal128", optional: true },
-        optUuid: { type: "uuid", optional: true },
-        optBool: { type: "bool", optional: true },
-        optStringList: { type: "list", optional: true, objectType: "string" },
+        _id: { type: 'objectId', indexed: true },
+        manNumber: { type: 'int', indexed: true },
+        manString: { type: 'string' },
+        optDecimal: { type: 'decimal128', optional: true },
+        optUuid: { type: 'uuid', optional: true },
+        optBool: { type: 'bool', optional: true },
+        optStringList: { type: 'list', optional: true, objectType: 'string' },
         optEmbeddedObjects: {
-          type: "list",
+          type: 'list',
           optional: true,
-          objectType: "EmbeddedModel",
+          objectType: 'EmbeddedModel',
         },
-        items: { type: "Item" },
+        items: { type: 'Item' },
       },
-      primaryKey: "_id",
-      name: "TestModel",
-    });
+      primaryKey: '_id',
+      name: 'TestModel',
+    })
 
     // @ts-ignore
     expect(Item.schema).toStrictEqual({
       properties: {
-        _id: { type: "objectId", indexed: true },
+        _id: { type: 'objectId', indexed: true },
         models: {
-          type: "linkingObjects",
-          objectType: "TestModel",
-          property: "items",
+          type: 'linkingObjects',
+          objectType: 'TestModel',
+          property: 'items',
         },
       },
-      primaryKey: "_id",
-      name: "Item",
-    });
-  });
-});
+      primaryKey: '_id',
+      name: 'Item',
+    })
+  })
+})
--- src/utils.ts
+++ src/utils.ts
@@ -9,30 +9,30 @@
 //   sw
 // }
 
-export const getRealmType = (type: string) => {
+export const getRealmType = (type: string): string => {
   switch (type) {
-    case "Date":
-      return "date";
-    case "String":
-      return "string";
-    case "Boolean":
-      return "bool";
-    case "Number":
-      return "double"; // default to double for numbers, can be overridden in decorator.
+    case 'Date':
+      return 'date'
+    case 'String':
+      return 'string'
+    case 'Boolean':
+      return 'bool'
+    case 'Number':
+      return 'double' // default to double for numbers, can be overridden in decorator.
 
     // TODO: case 'DATA' // no such thing...
 
     // BSON
-    case "Decimal128":
-      return "decimal128";
-    case "ObjectId":
-      return "objectId";
-    case "UUID":
-      return "uuid";
+    case 'Decimal128':
+      return 'decimal128'
+    case 'ObjectId':
+      return 'objectId'
+    case 'UUID':
+      return 'uuid'
 
     // TODO: should we handle Arrays/Lists here?
 
     default:
-      return type;
+      return type
   }
-};
+}
